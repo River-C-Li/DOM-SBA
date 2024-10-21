a383d041b49d6d7e413172feddbbe9bb1005ed17
River-C-Li <chengrui8848@gmail.com>
2024-10-21 18:20:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -361,3 +361,38 @@ requote.addEventListener("click", function () {
 });
 // Close the document stream
 myWindow.document.close();
+
+
+
+// clone:
+const ordersummary = document.getElementById("ordersummary");
+
+// A builder function to make the
+// created DOM structures consistent.
+function createPost(title, content) {
+  // Using an HTML template clone
+  const postTemplate = document.getElementById("postTemplate");
+  const clone = postTemplate.content.cloneNode(true);
+
+  // We can use selectors to find different nodes
+  // within the template clone itself.
+  const heading = clone.getElementById("clonet");
+  const body = clone.getElementById("clonep");
+
+  heading.textContent = title;
+  body.innerHTML = content;
+
+  return clone;
+}
+
+// Now, we can use the function to build consistent
+// post elements using custom data. We'll be pulling
+// from the JSON Placeholder API for convenience.
+(async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  posts.forEach((post) => {
+    ordersummary.appendChild(createPost(post.title, post.body));
+  });
+})();
